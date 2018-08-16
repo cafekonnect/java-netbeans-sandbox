016d530d98c144521ed7400beb63f1a2307b5947
Christopher Kyalo Mulei <38585737+cafekonnect@users.noreply.github.com>
2018-08-16 15:13:19 +0300

    Add files via upload

---
+++ TimeClass.java
@@ -0,0 +1,19 @@
+
+package pkg36.timeclass;
+
+/**
+ *
+ * @author CHRIS
+ */
+public class TimeClass {
+
+    public static void main(String[] args) {
+apples applesObj=new apples();
+System.out.println(applesObj.toMilitary());
+applesObj.setTime(13,24,6);
+System.out.println(applesObj.toMilitary());
+System.out.println(applesObj.toString());
+
+    }
+    
+}
